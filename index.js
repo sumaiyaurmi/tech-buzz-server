689bf6c692b617031bb5835149bef51e641060d8
Sumaiya Urmi <akthersumaiya689@gmail.com>
2024-06-10 20:06:33 +0600

    jwt token  implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,9 +29,33 @@ async function run() {
     const productssCollection = client.db("techBuzzDB").collection("productsss");
     const trendingCollection = client.db("techBuzzDB").collection("trendings");
 
-    // products apis
+    // jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
 
-    // get all submitted assignment by Specefic User
+     //  middlewares
+     const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        console.log(decoded);
+        next();
+      });
+    };
+
+    // products apis
     app.get("/products", async (req, res) => {
       const result = await productssCollection.find().toArray();
       res.send(result);
